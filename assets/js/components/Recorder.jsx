fde3a5cf4a4d42858eec0704c4e113ad8dfe532e
Andrew Jesaitis <andrew@andrewjesaitis.com>
2016-10-13 17:05:30 -0600

    Recording UI

--- assets/js/components/Recorder.jsx
+++ assets/js/components/Recorder.jsx
@@ -1,18 +1,38 @@
 import React, { PropTypes } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
-function Recorder({ showModal, src, onClose }) {
+function Recorder({ showModal, isRecording, hasRecording, onRecord,
+                    src, onClose, onPost, autoPlay, muted,
+                    controls, loop, width, height }) {
   return (
     <Modal show={showModal}>
+      <Modal.Header>
+        <button type="button" className="close" aria-label="Close" onClick={onClose}>
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </Modal.Header>
       <Modal.Body>
         <div className="row">
-          <div className="col-sm-12">
-            <video autoPlay muted src={src} />
+          <div className="col-sm-12 text-center">
+            <video 
+              autoPlay={autoPlay}
+              muted={muted}
+              controls={controls}
+              loop={loop}
+              width={width}
+              height={height}
+              src={src} 
+            />
           </div>
         </div>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={onClose}>Close</Button>
+        <Button className="btn-primary" disabled={!hasRecording} onClick={onPost}>
+          Post
+        </Button>
+        <Button className="btn-danger" active={isRecording} onClick={onRecord}>
+          { isRecording ? "Stop" : "Record" }
+        </Button>
       </Modal.Footer>
     </Modal>
   );
@@ -20,8 +40,18 @@ function Recorder({ showModal, src, onClose }) {
 
 Recorder.propTypes = {
   showModal: PropTypes.bool.isRequired,
+  isRecording: PropTypes.bool.isRequired,
+  hasRecording: PropTypes.bool.isRequired,
+  onRecord: PropTypes.func.isRequired,
   src: PropTypes.string.isRequired,
   onClose: PropTypes.func.isRequired,
+  onPost: PropTypes.func.isRequired,
+  autoPlay: PropTypes.bool.isRequired,
+  muted: PropTypes.bool.isRequired,
+  controls: PropTypes.bool.isRequired,
+  loop: PropTypes.bool.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
 };
 
 export default Recorder;
--- assets/js/containers/RecorderContainer.jsx
+++ assets/js/containers/RecorderContainer.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import RecordRTC from 'recordrtc';
 import Recorder from '../components/Recorder';
 
 class RecorderContainer extends Component {
@@ -8,7 +9,15 @@ class RecorderContainer extends Component {
       s3Url: '',
       src: '',
       showModal: true,
+      isRecording: false,
+      hasRecording: false,
       recordVideo: null,
+      autoPlay: true,
+      muted: true,
+      controls: false,
+      loop: false,
+      width: 480,
+      height: 360,
     };
   }
   componentDidMount() {
@@ -25,14 +34,40 @@ class RecorderContainer extends Component {
       console.warn(JSON.stringify(err));
     });
   }
-  handleStartRecord() {
+  handleRecord() {
+    if (this.state.isRecording) {
+      this.stopRecording();
+    } else {
+      this.startRecording();
+    }
+  }
+  startRecording() {
     this.captureUserMedia((stream) => {
-      this.setState({ recordVideo: RecordRTC(stream, {type: 'video'}) });
+      this.setState({
+        isRecording: true,
+        muted: true,
+        loop: false,
+        controls: true,
+        src: window.URL.createObjectURL(stream),
+        recordVideo: new RecordRTC(stream, { type: 'video' }),
+      });
       this.state.recordVideo.startRecording();
     });
   }
-  handleStopRecord() {
-    this.recordVideo.stopRecording();
+  stopRecording() {
+    this.setState({
+      isRecording: false,
+      hasRecording: true,
+      loop: true,
+      controls: true,
+      muted: false,
+    });
+    this.state.recordVideo.stopRecording((videoUrl) => {
+      this.setState({ src: videoUrl });
+    });
+  }
+  handlePost() {
+    console.log("Uploading...");
   }
   handleClose() {
     this.setState({
@@ -43,8 +78,19 @@ class RecorderContainer extends Component {
     return (
       <Recorder
         showModal={this.state.showModal}
+        isRecording={this.state.isRecording}
+        hasRecording={this.hasRecording}
+        onRecord={() => this.handleRecord()}
+        onPost={() => this.handlePost()}
         src={this.state.src}
         onClose={() => this.handleClose()}
+        autoPlay={this.state.autoPlay}
+        muted={this.state.muted}
+        controls={this.state.controls}
+        loop={this.state.loop}
+        width={this.state.width}
+        height={this.state.height}
+
       />
     );
   }
