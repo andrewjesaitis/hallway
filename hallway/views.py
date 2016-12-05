61bebea3c6a167f367050cc1f066907804d1d6c3
Andrew Jesaitis <andrew@andrewjesaitis.com>
2016-12-05 14:55:46 -0700

    Fix redirect if user has no discussion group

--- hallway/views.py
+++ hallway/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, redirect
 def home_view(request, *args, **kwargs):
     if request.user.is_authenticated:
         dg = request.user.discussion_groups.first()
-        return redirect('discussion-group-view', pk=dg.pk)
+        if dg:
+            return redirect('discussion-group-view', pk=dg.pk)
+        else:
+            return redirect('profile')
         
     return render(request, 'index.html')
